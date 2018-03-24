7ade8032852081f52e8d227795da14af54b0d971
Rodrigo <rodrigoarreola96@gmail.com>
2018-03-24 01:03:08 -0600

    Project up to close all windows on quit

---
+++ main.js
@@ -0,0 +1,76 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow, Menu, globalShortcut} = electron;
+
+let mainWindow;
+let addWindow;
+
+// Listen for the app to be ready
+app.on('ready', function(){
+  // Create new window
+  mainWindow = new BrowserWindow({});
+  // Load the html file into the window
+  mainWindow.loadURL(url.format({
+    // loadURL works this way file://dirname/mainWindow.html
+    pathname: path.join(__dirname, 'mainWindow.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+  // Quit app when closed
+  mainWindow.on('close', function(){
+    app.quit();
+  })
+
+  // Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  // Insert the Menu
+  Menu.setApplicationMenu(mainMenu);
+  //Register a shortcut
+  globalShortcut.register('CommandOrControl+Q', () => {
+     app.quit();
+   })
+
+});
+
+// Handle create add Window
+function createAddWindow(){
+  // Create new window
+  addWindow = new BrowserWindow({
+    width: 400,
+    height: 200,
+    title: 'Add shopping list item'
+  });
+  // Load the html file into the window
+  addWindow.loadURL(url.format({
+    // loadURL works this way file://dirname/mainWindow.html
+    pathname: path.join(__dirname, 'addWindow.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+}
+
+// Create menu template
+const mainMenuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Add item',
+        click(){
+          createAddWindow();
+        }
+      },
+      {
+        label: 'Clear items'
+      },
+      {
+        label: 'Quit',
+        click(){
+          app.quit();
+        }
+      }
+    ]
+  }
+];
